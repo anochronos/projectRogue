3abc87a9c42047cc517c40b12244b64b4d364b21
Akshun <135464796+anochronos@users.noreply.github.com>
2023-08-02 18:56:16 -0400

    Update procgen to create tunnel

--- procgen.py
+++ procgen.py
@@ -1,4 +1,10 @@
-from typing import Tuple
+import random
+from typing import Iterator, Tuple
+
+import tcod
+
+from game_map import GameMap
+import tiles_types
 
 
 class RectangulareRoom:
@@ -19,3 +25,34 @@ class RectangulareRoom:
     def inner(self) -> Tuple[slice, slice]:
         """Return the inner area of this room as a 2D array index"""
         return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
+    
+def tunnel_between(
+    start: Tuple[int, int], end: Tuple[int, int]
+) -> Iterator[Tuple[int, int]]:
+    """Return an L-shaped tunnel between these two points"""
+    x1, y1 =  start
+    x2, y2 = end
+    if random.random() < 0.5:
+        # Move horizontally the vertically
+        corner_x, corner_y = x2, y1
+    else:
+        # Move vertically, then horizontally
+        corner_x, corner_y = x1, y2
+    
+    # Generate the coordinates for this tunnel
+    for x, y in tcod.los.bresenham((x1, y1), (corner_x, corner_y)).tolist():
+        yield x, y
+    for x, y in tcod.los.bresenham((corner_x, corner_y), (x2, y2)).tolist():
+        yield x, y
+    
+
+def generate_dungeon(map_width, map_height) -> GameMap:
+    dungeon = GameMap(map_width, map_height)
+
+    room_1 = RectangulareRoom(x=20, y=15, width=10, height=15)
+    room_2 = RectangulareRoom(x=35, y=15, width=10, height=15)
+
+    dungeon.tiles[room_1.inner] = tiles_types.floor
+    dungeon.tiles[room_2.inner] = tiles_types.floor
+
+    return dungeon
